04f5030aa1d90b8a59c5b13629885f96de6fecdc
Eli Cusic <tntitans31@gmail.com>
2020-11-23 09:16:01 -0600

    commit tree 7cfa5f2f47767dd1be23579bc7fcb6922de5e28e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function getPhotos(object){
+    const div = document.getElementById('dog-image-container')
+    object.forEach(function(value, key){
+        let child = document.createElement("img")
+        child.setAttribute("src", `${value}`)
+        div.appendChild(child)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch(imgUrl)
+    .then(function(response){
+        return response.json()
+    }).then(function(json){
+        getPhotos(json["message"])
+    })
+})
+
+
+
+
+
+
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
